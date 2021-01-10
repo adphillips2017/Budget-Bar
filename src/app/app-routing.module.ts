cfb0479a81f9bb217b103ec4948f78c837a803c2
adphillips2017 <adphillips2017@users.noreply.github.com>
2021-01-09 23:15:26 -0500

    working on getting theming set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './core/pages/login/login.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
