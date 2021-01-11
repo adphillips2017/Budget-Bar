11261689fc8ffe0e2179344be213364cc1c788a7
adphillips2017 <adphillips2017@users.noreply.github.com>
2021-01-10 19:10:03 -0500

    fresh start to fix some angular issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
-import { RouterModule, Routes } from '@angular/router';
-
-import { LoginComponent } from './core/pages/login/login.component';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'login', component: LoginComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
