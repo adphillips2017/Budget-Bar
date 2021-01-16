06425a3e974b14409db2fe25a3353925bfb19933
Donald Adamson <dadamson@lci1.com>
2021-01-16 17:50:29 -0500

    Litterally copy pasta from another branch

--- website/e2e/src/app.e2e-spec.ts
+++ website/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('nobleui-angular app is running!');
+  it('should display welcome message', async () => {
+    await page.navigateTo();
+    expect(await page.getTitleText()).toEqual('budgi app is running!');
   });
 
   afterEach(async () => {
--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'budgi-ui';
-
-  ngOnInit(): void {}
-
+export class AppComponent {
+  @HostBinding('class') readonly clsName = 'app-login';
+  title = 'budgi';
 }
